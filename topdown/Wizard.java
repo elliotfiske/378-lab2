216130b9dbf48a26a716866322ff054664706ba6
jacwkang <jacwkang@gmail.com>
2016-02-02 17:24:04 -0800

    Added basic WASD movements

--- topdown/Wizard.java
+++ topdown/Wizard.java
@@ -8,6 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Wizard extends Actor
 {
+    public int speed = 3;
     /**
      * Act - do whatever the Wizard wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,5 +16,37 @@ public class Wizard extends Actor
     public void act() 
     {
         // Add your action code here.
+        checkKeyMovement();
     }    
+    
+    public void checkKeyMovement() {
+        if (Greenfoot.isKeyDown("w")) {
+            moveUp();
+        }
+        else if (Greenfoot.isKeyDown("s")) {
+            moveDown();
+        }
+        else if (Greenfoot.isKeyDown("a")) {
+            moveLeft();
+        }
+        else if (Greenfoot.isKeyDown("d")) {
+            moveRight();
+        }
+    }
+    
+    public void moveLeft() {
+        setLocation(getX() - speed, getY());
+    }
+    
+    public void moveRight() {
+        setLocation(getX() + speed, getY());
+    }
+    
+    public void moveUp() {
+        setLocation(getX(), getY() - speed);
+    }
+    
+    public void moveDown() {
+        setLocation(getX(), getY() + speed);
+    }
 }

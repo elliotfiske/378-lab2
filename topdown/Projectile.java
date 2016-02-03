971c840b87f22908f8c3293b9898c70c039ccebb
dualer208 <dualer208@yahoo.com>
2016-02-02 23:34:54 -0800

    revise 1 of projectile

--- topdown/Projectile.java
+++ topdown/Projectile.java
@@ -6,13 +6,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
+
+
+
 public class Projectile extends Actor
 {
-    int mx;
-    int my;
+    private int mx;
+    private int my;
     
-    int life = 200;
-    int speed = 3;
+    private int dist = 400; //distance aka life = dist/speed
+    private int speed = 3;
+    private int delay = 50; //time between each fire
+    private int timer = 0;
+    
+    public Projectile()
+    {
+        direction();
+    }
     
     private void direction()
     {
@@ -21,23 +31,48 @@ public class Projectile extends Actor
         if(mouse!=null){
            mx = mouse.getX();
            my = mouse.getY();
+           //possible implementaiton after human
+           //setRotation((int)(180*Math.atan2(mouse.getY()-getY(),mouse.getX()-getX())/Math.PI));
+        }
+        
+    }
+    
+    private void fire()
+    {
+        if("space".equals(Greenfoot.getKey()))//(Greenfoot.isKeyDown("space") && timer == delay)
+        {
+            getWorld().addObject(new Projectile(), 1, 1);
+            timer = 0;
+        }
+        //else
+        //     timer++;
+    }
+    
+    private void move()
+    {
+        move(speed);
+        dist -= speed;
+    }
+    
+    private void remove()
+    {
+        if(dist <= 0 || this.isAtEdge())
+        { 
+            World world;
+            world = getWorld();
+            world.removeObject(this);
         }
     }
     
     public void act() 
     {
         super.act();
-        
-        Actor actor = getOneIntersectingObject(Actor.class);
+        fire();
         
         turnTowards(mx, my);
-        move(speed);
-        life -= speed;
+        move();
         
-        if(life <= 0 || this.isAtEdge())
-        {
-            getWorld().removeObject(this);
-        }
+        remove();
     }    
     
 }
